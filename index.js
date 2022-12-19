9e76f29d274363747798136e7ca10ec85b7d095d
aziznurulloh8910 <aziznurulloh8@gmail.com>
2022-12-19 23:22:58 +0700

    chg: vercel

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const routes = require('./src/routes/index.js')
 const app = express()
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.use(express.static('public'))
 
 app.use('/', routes)
 
-// app.use('/404', (req, res) => {
-//   return res.status(404).send({
-//     success: false,
-//     message: 'Resource not found'
-//   })
-// })
+app.use('/404', (req, res) => {
+  return res.status(404).send({
+    success: false,
+    message: 'Resource not found'
+  })
+})
 
-// app.use('*', (req, res) => {
-//   return res.redirect('/404')
-// })
+app.use('*', (req, res) => {
+  return res.redirect('/404')
+})
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(`App listening on port ${process.env.PORT || 3333}`)
